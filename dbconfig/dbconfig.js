a8d5436197c780034a8ecfb8199764cb6ee33a6e
Sai Krishna Gummadidala <saikrishnagummadidala34@gmail.com>
2026-03-16 10:26:36 +0530

    Backend ready for Render

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -4,11 +4,10 @@ require('dotenv').config();
 exports.dbconnect = () => {
     mongoose.connect(process.env.dburl)
         .then(() => {
-            console.log('Connection is successed!');
+            console.log("Connection is successed!");
         })
         .catch((err) => {
-            console.log('Database Connection Failed');
+            console.log("Database Connection Failed");
             console.error(err.message);
-            process.exit(1);
         });
 };
